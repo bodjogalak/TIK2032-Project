ae68847d1fe3aa89d704f8a246f8015977b22bdb
bodjogalak <substomrbeast6000@gmail.com>
2024-04-12 19:58:13 +0700

    menambahkan file javascript ke repo

---
+++ jeremia.js
@@ -0,0 +1,11 @@
+const nama = "jeremia pasca";
+let usia = 20;
+
+console.log("nama saya adalah ", nama, "usia saya adalah", usia);
+function generateBiodata() {
+  console.log(`nama saya adalah ${nama} dan usia saya ${usia} tahun`);
+}
+
+console.log(nama);
+
+console.log(usia);
