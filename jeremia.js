eb0a85d37988138fd9418bf197161b980010d686
bodjogalak <163124933+bodjogalak@users.noreply.github.com>
2024-04-15 14:43:11 +0700

    mengubah paragraf function komputerlokal dan mengubah timeset 5000Update jeremia.js

--- jeremia.js
+++ jeremia.js
@@ -28,10 +28,11 @@ function gambargallery3() {
 window.open("latihan.jpg");
 }
 console.time("timeout");
+
+  // looping buat komputer lokal untuk Get ke server
+for (let i = 0; i < 100000; i++) {
+setTimeout(komputerlokal,5000);       
+}
 function komputerlokal() {
   console.log("reply from server 200");
 }  
-  // looping buat komputer lokal untuk Get ke server
-for (let i = 0; i < 100000; i++) {
-setTimeout(komputerlokal,10000);       
-}
