224e2bd187a41312e1bac801a9813e8d05097deb
bodjogalak <163124933+bodjogalak@users.noreply.github.com>
2024-04-21 22:05:52 +0700

    cba gnti syntax tidak pkai variable dataduaUpdate jeremia.js

--- jeremia.js
+++ jeremia.js
@@ -17,8 +17,8 @@ function gambarjeremia2() {
   window.open("jeremia2.jpg");
 }
 var datadua = document.querySelector(".gmbr1");
-console.log(datadua);
-window.addEventListener.datadua('click', function (gambargallery1) {
+console.log(document.querySelector(".gmbr1");
+document.querySelector(".gmbr1").window.addEventListener('click', function (gambargallery1) {
   window.open("jeremia3.jpg");
 });
 
