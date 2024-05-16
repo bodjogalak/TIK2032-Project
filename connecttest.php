580a2cb0fe92f6dcd5feed816d38dd854d0617d3
bodjogalak <substomrbeast6000@gmail.com>
2024-05-17 00:47:30 +0700

    menambahkan file php bru

---
+++ connecttest.php
@@ -0,0 +1,40 @@
+<?php
+
+ // koneksi ke database
+$koneksi = mysqli_connect("127.0.0.1:3306", "root", "" , "teknik");
+
+
+
+
+   // melakukan perintah querry pda database
+$hasil = mysqli_query($koneksi, "SELECT * FROM mahasiswa");
+
+$data = mysqli_fetch_assoc($hasil);
+if ($hasil) {
+
+echo "Isi data mahasiswa: " . "<br>";
+    
+  // fetch atau mengambil isi datanya dri database
+while ($isidatanya = mysqli_fetch_assoc($hasil)) {
+  
+    var_dump($isidatanya);
+     echo "<br>";
+
+    
+
+   }
+   echo " " . "<br>";
+
+   echo " " . "<br>";
+
+   echo " " . "<br>";
+
+ echo "ini adalah batas header";
+
+    // jika gagal mendapatkan data dri database
+} else if(!$hasil) {
+
+echo mysqli_error($hasil);
+
+}
+?>
